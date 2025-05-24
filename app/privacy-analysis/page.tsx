3f8d699d8919ece9b4422ea7538a8cbd03f7a68c
f-xdu-23-computer science <145787555+fbb24@users.noreply.github.com>
2025-05-24 15:36:41 +0800

    tsx文件

---
+++ app/privacy-analysis/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const IframePage = () => (
+
+
+  <div>
+
+
+    <iframe src="/index.html" width="100%" height="1100" title="My HTML Page"></iframe>
+
+
+  </div>
+
+
+);
+
+
+export default IframePage;
