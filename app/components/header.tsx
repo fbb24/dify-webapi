9d7c80e63bb87754bffc75078c2c9be32f7c3e71
f-xdu-23-computer science <145787555+fbb24@users.noreply.github.com>
2025-05-25 16:25:53 +0800

    gai

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -18,7 +18,9 @@ const Header: FC<IHeaderProps> = ({
   onCreateNewChat,
 }) => {
   return (
+
     <div className="shrink-0 flex items-center justify-between h-14 px-3 bg-primary-100">
+
       {isMobile
         ? (
           <div
